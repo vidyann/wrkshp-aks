c57899f16fc0633af6a8ba55474e4a57453bf8df
Vidya Narasimhan <Vidya.Narasimhan@microsoft.com>
2020-08-29 21:26:05 +0000

    modified variables

--- 02_Manual_deploy_mongo_configure_secret.ps1
+++ 02_Manual_deploy_mongo_configure_secret.ps1
@@ -1,13 +1,23 @@
-$REGION_NAME= "eastus"
-$RESOURCE_GROUP= "aksworkshop01"
-$SUBNET_NAME= "aks-subnet"
-$VNET_NAME= "aks-vnet"
+#$REGION_NAME= "eastus"
+#$RESOURCE_GROUP= "aksworkshop10"
+#$SUBNET_NAME= "aks-subnet10"
+#$VNET_NAME= "aks-vnet10"
 
 # update the name
-$WORKSPACE= ""
+#$WORKSPACE= "aksworkshop-workspace-Github"
+
+REGION_NAME=eastus
+RESOURCE_GROUP=aksworkshop10
+SUBNET_NAME=aks-subnet10
+VNET_NAME=aks-vnet10
+
+# update the name
+WORKSPACE=aksworkshop-workspace-Github
 
 # change with your cluster name 
-$AKS_CLUSTER_NAME= "" 
+#$AKS_CLUSTER_NAME= "aksworkshop-Github" 
+AKS_CLUSTER_NAME=aksworkshop-Github 
+
 
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 
@@ -16,10 +26,10 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm search repo bitnami
 
 # change with your user name and password 
-helm install ratings bitnami/mongodb --namespace ratingsapp --set auth.username=<user name>,auth.password=<your password>,auth.database=ratingsdb
+helm install ratings bitnami/mongodb --namespace ratingsapp --set auth.username=mongoadmin,auth.password=mongopass,auth.database=ratingsdb
 
 # change with your user name and password 
-kubectl create secret generic mongosecret --namespace ratingsapp --from-literal=MONGOCONNECTION="mongodb://<user name>:<your password>@ratings-mongodb.ratingsapp:27017/ratingsdb"
+kubectl create secret generic mongosecret --namespace ratingsapp --from-literal=MONGOCONNECTION="mongodb://mongoadmin:mongopass@ratings-mongodb.ratingsapp:27017/ratingsdb"
 
 kubectl describe secret mongosecret --namespace ratingsapp
 
